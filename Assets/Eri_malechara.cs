380f7af371d1bc7b9df56fb81b6bb40f786ef991
ErinaNg <nlqerina15@gmail.com>
2023-02-16 20:28:11 +0800

    Initial commit

---
+++ Assets/Eri_malechara.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eri_malechara : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    private Vector3 moveDirection;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveZ = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(0, 0, moveZ);
+        
+        if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))  //Walk
+        {
+            Walk();
+        }
+        else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)) //Run
+        {
+            Run();
+        }
+        else if(moveDirection == Vector3.zero) //no movement //idle
+        {
+            Idle();
+        }
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    private void Idle()
+    {
+
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+    }
+}
---
+++ Assets/Erina/Eri_playerscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eri_playerscript : MonoBehaviour
+{
+    public float moveSpeed = 7;
+    public float smoothMoveTime = .1f;
+
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+
+    public Animator anim;
+    void Update()
+    {
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        transform.eulerAngles = Vector3.up * targetAngle;
+
+        transform.Translate(transform.forward * moveSpeed * Time.deltaTime * smoothInputMagnitude, Space.World);
+    }
+}
