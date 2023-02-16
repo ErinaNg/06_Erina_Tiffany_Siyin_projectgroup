21ef401c1bc5eb69d2ca29618f6cd96bbed2ce09
NorrinaDrago <norrinaong@gmail.com>
2023-02-16 21:19:52 +0800

    I imported my file
    I imported my export unityfile assets and scripts

---
+++ Assets/TiffanyScript/TutorialKnightScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TutorialKnightScript : MonoBehaviour
+{
+    public static bool IsActive;
+    private NavMeshAgent navMeshAgent;
+    public static TutorialKnightScript TutorialKnight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        IsActive = true;
+        TutorialKnight = this;
+    }
+
+    public void KnightWalksToDestination()
+    {
+
+    }
+}
---
+++ Assets/TiffanyScript/TutorialScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour
+{
+    public Text NarratorText;
+    public GameObject TutorialKnight;
+    public GameObject FoodTray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FoodTray.active = false;
+        NarratorText.text = "Hello Timmy II! It seems you are in prison, like usual.";
+        Invoke("KnightShowUpText", 3.0f);
+    }
+
+    void KnightShowUpText()
+    {
+        NarratorText.text = "Oh Look the knight here to give your your daily lunch.";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
