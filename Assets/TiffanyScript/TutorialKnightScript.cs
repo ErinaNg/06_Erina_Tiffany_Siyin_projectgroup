8a469b93aa5bec99bc532a4910776eaa15c7e769
NorrinaDrago <norrinaong@gmail.com>
2023-02-17 23:06:17 +0800

    Still working on tutorial, stop for the day

--- Assets/TiffanyScript/TutorialKnightScript.cs
+++ Assets/TiffanyScript/TutorialKnightScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class TutorialKnightScript : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class TutorialKnightScript : MonoBehaviour
     private Vector3 Destination;
     public GameObject FoodTray;
     public float DestinationTimer;
+    public GameObject Door;
+    public Text NarratorText;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +25,19 @@ public class TutorialKnightScript : MonoBehaviour
         TutorialKnightCode = this;
         FoodTray.active = false;
         DestinationTimer = 0;
+        NarratorText.text = "Hello Timmy II! It seems you are in prison, like usual.";
+        Invoke("KnightShowUpText", 3.5f);
+    }
+
+    void KnightShowUpText()
+    {
+        NarratorText.text = "Oh Look! The knight here to give your your daily lunch.";
+        TutorialKnightCode.KnightWalksToDestination();
     }
 
     public void KnightWalksToDestination()
     {
+        Door.transform.Rotate(Door.transform.rotation.x, 90f, Door.transform.rotation.y);
         float DestinationZ = -8.63f;
         float DestinationX = 23.98f;
         Destination = new Vector3(DestinationX, transform.position.y, DestinationZ);
@@ -43,6 +55,9 @@ public class TutorialKnightScript : MonoBehaviour
     {
         animator.SetBool("EnemyRunning", false);
         FoodTray.active = true;
+; 
+        Vector3 Turn = new Vector3(transform.rotation.x, 270f, transform.rotation.z);
+        Quaternion tempRot = transform.rotation = Quaternion.LookRotation(Turn);
     }
 
     void Update()
