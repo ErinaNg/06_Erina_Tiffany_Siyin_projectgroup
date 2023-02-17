e736bddba02fc8c5c9265b21e32a33bb6f5410fb
StudentAFK <122333763+StudentAFK@users.noreply.github.com>
2023-02-17 17:18:26 +0800

    Add files via upload
    c# script

---
+++ sceneSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class sceneSwitch : MonoBehaviour
+{
+    public Image playbtn;
+
+    public void playScene()
+    {
+        SceneManager.LoadScene("startScene");
+    }
+}
