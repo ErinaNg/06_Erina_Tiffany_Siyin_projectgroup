2545ea6a54c57ec63b1dc2877ab7d01d4290c8c9
ErinaNg <nlqerina15@gmail.com>
2023-02-19 20:28:47 +0800

    Add Jail door asset did auto anim, script and sound effect by Erina
    Door assets, done scripting for door added sound effect too

---
+++ Assets/Erina/Eri_doorcontroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eri_doorcontroller : MonoBehaviour
+{
+    public int id;
+    private void Start()
+    {
+        GameEvents.current.onDoorwayTriggerEnter += OnDoorwayOpen; //acess action //declare action as events
+        GameEvents.current.onDoorwayTriggerEnter += OnDoorwayClose;
+    }
+
+    private void OnDoorwayClose(int id)
+    {
+        if (id == this.id)
+        {
+            LeanTween.moveLocalY(gameObject, .75f, 1f).setEaseInQuad();
+        }
+
+    }
+    private void OnDoorwayOpen(int id)
+    {
+        if (id == this.id)
+        {
+            LeanTween.moveLocalY(gameObject, 1.6f, 1f).setEaseOutQuad();
+        }
+           
+    }
+
+   
+}
