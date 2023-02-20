b6e3ef3fdecd067b3b60e45b0f4fdbf66fd24bc1
ErinaNg <nlqerina15@gmail.com>
2023-02-20 15:01:34 +0800

    Revert "Delete Assets directory"
    This reverts commit d3c8a8e2e9e2b14d03604ab3e48b20abf72408fa.

---
+++ Assets/Erina/EriScripts/EriDoorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EriDoorScript : MonoBehaviour
+{
+    public Animator animatorDoor;
+    private AudioSource audioSource;
+    [SerializeField] private AudioSource doorSound;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Start()
+    {
+        animatorDoor = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animatorDoor.SetBool("Open",true);
+         
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animatorDoor.SetBool("Open", false);
+            doorSound.Play();
+        }
+    }
+}
---
+++ Assets/Erina/EriScripts/GameEvents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour //Eri
+{
+    public static GameEvents current;
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action<int> onDoorwayTriggerEnter;
+    public void DoorwayTriggerEnter(int id)
+    {
+        if(onDoorwayTriggerEnter != null)
+        {
+            onDoorwayTriggerEnter(id);
+        }
+    }
+    public event Action<int> onDoorwayTriggerExit;
+    public void DoorwayTriggerExit(int id)
+    {
+        if(onDoorwayTriggerExit != null)
+        {
+            onDoorwayTriggerExit(id);
+        }
+    }
+
+}
